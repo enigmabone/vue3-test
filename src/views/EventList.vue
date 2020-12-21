a3f5cc4cf3c56cf6d7d1d78feabd092963b0dd81
Kyle Miller <kylemiller@Kyles-MBP.home>
2020-12-21 08:37:10 -0600

    lots  changes

---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="events">
+   <h1>Events List</h1>
+   <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard"
+import EventService from "@/services/EventService.js"
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+events: null
+    }
+  },
+  created() {
+    EventService.getEvents().then(response => {
+      this.events = response.data
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+};
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
